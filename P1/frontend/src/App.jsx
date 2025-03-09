fb63057184dba2b071ab9bad6a13ed7caa9e1130
yodak025 <diego.sota@gmail.com>
2025-03-09 18:37:21 +0100

    Footer arreglado y estética mejorada

--- P1/frontend/src/App.jsx
+++ P1/frontend/src/App.jsx
@@ -12,19 +12,33 @@ function App() {
         <header className='os-pageFront'>
           <h1>FRONT PAGE</h1>
         </header>
-        <Category name="Ciencia e Ingeniería">
-          <Product name="name 1" logoSrc="../public/vite.svg" index="1"/>
-          <Product name="name 2" logoSrc="../src/assets/react.svg" index="2"/>
+        <Category name="Empleo">
+          <Product name="Currículum Vitae Personalizado" logoSrc="../public/vite.svg" index="1"/>
+          <Product name="Carta de Presentación" logoSrc="../src/assets/react.svg" index="2"/>
           <Product name="name 3" description="Description" price="NaN" index="3"/>
           <Product name="name 4" description="Description" price="NaN" index="4"/>
           <Product name="name 5" description="Description" price="NaN" index="5"/>
           <Product name="name 6" description="Description" price="NaN" index="6"/>
           <Product name="name 7" description="Description" price="NaN" index="7"/>
         </Category>
-        <Category name="Empleo">
-          <Product name="name 1" description="Description" price="NaN" index="1"/>
-          <Product name="name 2" description="Description" price="NaN" index="2"/>
-          <Product name="name 3" description="Description" price="NaN" index="3"/>
+        <Category name="Ventas">
+          <Product name="Descripción de Producto Optimizada Para SEO" description="Description" price="NaN" index="1"/>
+          <Product name="Email de Ventas" description="Description" price="NaN" index="2"/>
+        </Category> 
+        <Category name="Ciencia e Ingeniería">
+          <Product name="Abstract Científico" description="Description" price="NaN" index="1"/>
+        </Category> 
+        <Category name="Empresa">
+          <Product name="Resumen Ejecutivo" description="Description" price="NaN" index="1"/>
+        </Category> 
+        <Category name="Redes Sociales">
+          <Product name="Guión para vídeo de Youtube" description="Description" price="NaN" index="1"/>
+          <Product name="Descripción para Youtube" description="Description" price="NaN" index="2"/>
+          <Product name="Descripción para Instagram" description="Description" price="NaN" index="3"/>
+        </Category> 
+        <Category name="Estudiantes">
+          <Product name="Tarjetas anki" description="Description" price="NaN" index="1"/>
+          <Product name="Revisar Redacción" description="Description" price="NaN" index="2"/>
         </Category> 
     </Layout>
     </>
