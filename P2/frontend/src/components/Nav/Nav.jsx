25b60d63c3c3e20ab361ec793135709e2f1a916c
yodak025 <diego.sota@gmail.com>
2025-04-06 13:35:00 +0200

    Cambios en Nav a medio hacer

--- P2/frontend/src/components/Nav/Nav.jsx
+++ P2/frontend/src/components/Nav/Nav.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './Nav.css';
 
-export default function Nav({className}) {
+function StaticNav({className}) {
 
   return (
     <nav className={className + ' os-nav'}>
@@ -13,6 +13,27 @@ export default function Nav({className}) {
           <a href="index.html#products">Productos</a>
           <a href="index.html">Sobre Nosotros</a>
       </main>
+    </nav>
+  );
+}
+
+export default function Nav({className}) {
+
+  return (
+    <nav className={className + ' os-nav'}>
+      <header className="os-nav-header">
+        <p>Menú de Navegación</p>
+        <a href="index.html#products">Productos</a>
+        <a href="index.html">Sobre Nosotros</a>
+      </header>
+
+      <section>
+          <input type="text" placeholder="Buscar..." />
+      </section>
+      <section className="os-nav-options">
+          <img src="" alt="Logo" className="os-nav-github" />
+          <a>Usuario</a>
+      </section>
     </nav>
   );
 }
